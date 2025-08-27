7202e2ece5dd93787aec55ef7a99fba83dc372d9
Lav Vadnagara <lav.vadnagara@adit.com>
2025-08-27 18:22:20 +0530

    sdfsddfdsdfvsdfv

---
+++ constantPatientDataRef.ts
@@ -0,0 +1,413 @@
+export const constantPatientData = {
+  dental: {
+    patient_01_Ruth_Canal: {
+      id: "01dental-a123-b456-c789-d101112e1301",
+      first_name: "Ruth",
+      last_name: "Canal",
+      display_name: "Ruth Canal",
+      user_name: "ruth_canal",
+      email: "ruth.canal@gmail.com",
+      mobile: "8323561581"
+    },
+    patient_02_Eve_Namel: {
+      id: "02dental-a123-b456-c789-d101112e1302",
+      first_name: "Eve",
+      last_name: "Namel",
+      display_name: "Eve Namel",
+      user_name: "eve_namel",
+      email: "eve.namel@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_03_Dan_Ture: {
+      id: "03dental-a123-b456-c789-d101112e1303",
+      first_name: "Dan",
+      last_name: "Ture",
+      display_name: "Dan Ture",
+      user_name: "dan_ture",
+      email: "dan.ture@gmail.com",
+      mobile: "3693279399"
+    },
+    patient_04_Perry_O_Dontist: {
+      id: "04dental-a123-b456-c789-d101112e1304",
+      first_name: "Perry",
+      last_name: "O'Dontist",
+      display_name: "Perry O'Dontist",
+      user_name: "perry_odontist",
+      email: "perry.odontist@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_05_Pearl_White: {
+      id: "05dental-a123-b456-c789-d101112e1305",
+      first_name: "Pearl",
+      last_name: "White",
+      display_name: "Pearl White",
+      user_name: "pearl_white",
+      email: "pearl.white@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_06_Miley_Smiley: {
+      id: "06dental-a123-b456-c789-d101112e1306",
+      first_name: "Miley",
+      last_name: "Smiley",
+      display_name: "Miley Smiley",
+      user_name: "miley_smiley",
+      email: "miley.smiley@gmail.com",
+      mobile: "3693279399"
+    },
+    patient_07_Ina_Cisor: {
+      id: "07dental-a123-b456-c789-d101112e1307",
+      first_name: "Ina",
+      last_name: "Cisor",
+      display_name: "Ina Cisor",
+      user_name: "ina_cisor",
+      email: "ina.cisor@gmail.com",
+      mobile: "6784312907"
+    },
+    patient_08_Annie_Steshia: {
+      id: "08dental-a123-b456-c789-d101112e1308",
+      first_name: "Annie",
+      last_name: "Steshia",
+      display_name: "Annie Steshia",
+      user_name: "annie_steshia",
+      email: "annie.steshia@gmail.com",
+      mobile: "3693279400"
+    },
+    patient_09_Sarah_Bellum: {
+      id: "09dental-a123-b456-c789-d101112e1309",
+      first_name: "Sarah",
+      last_name: "Bellum",
+      display_name: "Sarah Bellum",
+      user_name: "sarah_bellum",
+      email: "sarah.bellum@gmail.com",
+      mobile: "3693279401"
+    },
+    patient_10_Ernest_Flossy: {
+      id: "10dental-a123-b456-c789-d101112e1310",
+      first_name: "Ernest",
+      last_name: "Flossy",
+      display_name: "Ernest Flossy",
+      user_name: "ernest_flossy",
+      email: "ernest.flossy@gmail.com",
+      mobile: "7543279399"
+    },
+    patient_11_Phil_Ling: {
+      id: "11dental-a123-b456-c789-d101112e1311",
+      first_name: "Phil",
+      last_name: "Ling",
+      display_name: "Phil Ling",
+      user_name: "phil_ling",
+      email: "phil.ling@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_12_Moe_Lars: {
+      id: "12dental-a123-b456-c789-d101112e1312",
+      first_name: "Moe",
+      last_name: "Lars",
+      display_name: "Moe Lars",
+      user_name: "moe_lars",
+      email: "moe.lars@gmail.com",
+      mobile: "5793279399"
+    },
+    patient_13_Joe_Adam: {
+      id: "13dental-a123-b456-c789-d101112e1313",
+      first_name: "Joe",
+      last_name: "Adam",
+      display_name: "Joe Adam",
+      user_name: "joe_adam",
+      email: "joe.adam@gmail.com",
+      mobile: "654-123-7891"
+    },
+    patient_14_Mandi_Bill: {
+      id: "14dental-a123-b456-c789-d101112e1314",
+      first_name: "Mandi",
+      last_name: "Bill",
+      display_name: "Mandi Bill",
+      user_name: "mandi_bill",
+      email: "mandi.bill@gmail.com",
+      mobile: "3693279402"
+    },
+    patient_15_Ginger_Vitis: {
+      id: "15dental-a123-b456-c789-d101112e1315",
+      first_name: "Ginger",
+      last_name: "Vitis",
+      display_name: "Ginger Vitis",
+      user_name: "ginger_vitis",
+      email: "ginger.vitis@gmail.com",
+      mobile: "2321561581"
+    }
+  },
+  optometry: {
+    patient_01_Brittney_Vision: {
+      id: "018e6525-opto-b456-c789-d101112e1301",
+      first_name: "Brittney",
+      last_name: "Vision",
+      display_name: "Brittney Vision",
+      user_name: "brittney_vision",
+      email: "brittney.vision@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_02_Cee_Vision: {
+      id: "028e6525-opto-b456-c789-d101112e1302",
+      first_name: "Cee",
+      last_name: "Vision",
+      display_name: "Cee Vision",
+      user_name: "cee_vision",
+      email: "cee.vision@gmail.com",
+      mobile: "4693279391"
+    },
+    patient_03_Crystal_Clearview: {
+      id: "038e6525-opto-b456-c789-d101112e1303",
+      first_name: "Crystal",
+      last_name: "Clearview",
+      display_name: "Crystal Clearview",
+      user_name: "crystal_clearview",
+      email: "crystal.clearview@gmail.com",
+      mobile: "8323561581"
+    },
+    patient_04_Ellie_O_Vision: {
+      id: "048e6525-opto-b456-c789-d101112e1304",
+      first_name: "Ellie O.",
+      last_name: "Vision",
+      display_name: "Ellie O. Vision",
+      user_name: "ellie_o.vision",
+      email: "ellie.o.vision@gmail.com",
+      mobile: "6784312907"
+    },
+    patient_05_Gail_L_Fogg: {
+      id: "058e6525-opto-b456-c789-d101112e1305",
+      first_name: "Gail L.",
+      last_name: "Fogg",
+      display_name: "Gail L. Fogg",
+      user_name: "gail_l.fogg",
+      email: "gail.l.fogg@gmail.com",
+      mobile: "3693279399"
+    },
+    patient_06_Ima_Seewell: {
+      id: "068e6525-opto-b456-c789-d101112e1306",
+      first_name: "Ima",
+      last_name: "Seewell",
+      display_name: "Ima Seewell",
+      user_name: "ima_seewell",
+      email: "ima.seewell@gmail.com",
+      mobile: "6541237891"
+    },
+    patient_07_Ima_Squint: {
+      id: "078e6525-opto-b456-c789-d101112e1307",
+      first_name: "Ima",
+      last_name: "Squint",
+      display_name: "Ima Squint",
+      user_name: "ima_squint",
+      email: "ima.squint@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_08_Iris_Vision: {
+      id: "088e6525-opto-b456-c789-d101112e1308",
+      first_name: "Iris",
+      last_name: "Vision",
+      display_name: "Iris Vision",
+      user_name: "iris_vision",
+      email: "iris.vision@gmail.com",
+      mobile: "2321561581"
+    },
+    patient_09_Ima_Vision: {
+      id: "098e6525-opto-b456-c789-d101112e1309",
+      first_name: "Ima",
+      last_name: "Vision",
+      display_name: "Ima Vision",
+      user_name: "ima_vision",
+      email: "joe@yahoo.com",
+      mobile: "7543279399"
+    },
+    patient_10_Ophelia_Bright: {
+      id: "108e6525-opto-b456-c789-d101112e1310",
+      first_name: "Ophelia",
+      last_name: "Bright",
+      display_name: "Ophelia Bright",
+      user_name: "ophelia_bright",
+      email: "ophelia.bright@gmail.com",
+      mobile: "5693279399"
+    },
+    patient_11_Patricia_Sight: {
+      id: "118e6525-opto-b456-c789-d101112e1311",
+      first_name: "Patricia",
+      last_name: "Sight",
+      display_name: "Patricia Sight",
+      user_name: "patricia_sight",
+      email: "patricia@yahoo.com",
+      mobile: "2321561581"
+    },
+    patient_12_Paul_Vision: {
+      id: "128e6525-opto-b456-c789-d101112e1312",
+      first_name: "Paul",
+      last_name: "Vision",
+      display_name: "Paul Vision",
+      user_name: "paul_vision",
+      email: "paul.vision@gmail.com",
+      mobile: "5793279399"
+    },
+    patient_13_Peyton_L_Specter: {
+      id: "138e6525-opto-b456-c789-d101112e1313",
+      first_name: "Peyton L.",
+      last_name: "Specter",
+      display_name: "Peyton L. Specter",
+      user_name: "peyton_l.specter",
+      email: "peyton.l.specter@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_14_Ray_N_Shine: {
+      id: "148e6525-opto-b456-c789-d101112e1314",
+      first_name: "Ray N.",
+      last_name: "Shine",
+      display_name: "Ray N. Shine",
+      user_name: "ray_n.shine",
+      email: "ray.n.shine@gmail.com",
+      mobile: "4693279399"
+    },
+    patient_15_Seymore_Blurry: {
+      id: "158e6525-opto-b456-c789-d101112e1315",
+      first_name: "Seymore",
+      last_name: "Blurry",
+      display_name: "Seymore Blurry",
+      user_name: "seymore_blurry",
+      email: "seymore.blurry@gmail.com",
+      mobile: "4693279399"
+    }
+  },
+  chiropractic: {
+    patient_01_Ally_Vertebrae: {
+      id: "01chirop-a123-b456-c789-d101112e1301",
+      first_name: "Ally",
+      last_name: "Vertebrae",
+      display_name: "Ally Vertebrae",
+      user_name: "ally_vertebrae",
+      email: "ally.vertebrae@gmail.com",
+      mobile: "8323561581"
+    },
+    patient_02_Anita_Crack: {
+      id: "02chirop-a123-b456-c789-d101112e1302",
+      first_name: "Anita",
+      last_name: "Crack",
+      display_name: "Anita Crack",
+      user_name: "anita_crack",
+      email: "anita.crack@gmail.com",
+      mobile: "8323561582"
+    },
+    patient_03_Bend_E_Back: {
+      id: "03chirop-a123-b456-c789-d101112e1303",
+      first_name: "Bend E.",
+      last_name: "Back",
+      display_name: "Bend E. Back",
+      user_name: "bend_e.back",
+      email: "bend.e.back@gmail.com",
+      mobile: "8323561583"
+    },
+    patient_04_Bone_Care: {
+      id: "04chirop-a123-b456-c789-d101112e1304",
+      first_name: "Bone",
+      last_name: "Care",
+      display_name: "Bone Care",
+      user_name: "bone_care",
+      email: "bone.care@gmail.com",
+      mobile: "3693279399"
+    },
+    patient_05_Chiro_Spondylus: {
+      id: "05chirop-a123-b456-c789-d101112e1305",
+      first_name: "Chiro",
+      last_name: "Spondylus",
+      display_name: "Chiro Spondylus",
+      user_name: "chiro_spondylus",
+      email: "chiro.spondylus@gmail.com",
+      mobile: "3693279400"
+    },
+    patient_06_Flex_Spine: {
+      id: "06chirop-a123-b456-c789-d101112e1306",
+      first_name: "Flex",
+      last_name: "Spine",
+      display_name: "Flex Spine",
+      user_name: "flex_spine",
+      email: "flex.spine@gmail.com",
+      mobile: "3693279399"
+    },
+    patient_07_Justin_Alignment: {
+      id: "07chirop-a123-b456-c789-d101112e1307",
+      first_name: "Justin",
+      last_name: "Alignment",
+      display_name: "Justin Alignment",
+      user_name: "justin_alignment",
+      email: "justin.alignment@gmail.com",
+      mobile: "8323561585"
+    },
+    patient_08_Kneeda_Fix: {
+      id: "08chirop-a123-b456-c789-d101112e1308",
+      first_name: "Kneeda",
+      last_name: "Fix",
+      display_name: "Kneeda Fix",
+      user_name: "kneeda_fix",
+      email: "kneeda.fix@gmail.com",
+      mobile: "8323561587"
+    },
+    patient_09_Lacey_Bone: {
+      id: "09chirop-a123-b456-c789-d101112e1309",
+      first_name: "Lacey",
+      last_name: "Bone",
+      display_name: "Lacey Bone",
+      user_name: "lacey_bone",
+      email: "lacey.bone@gmail.com",
+      mobile: "8323561586"
+    },
+    patient_10_Leslie_Backus: {
+      id: "10chirop-a123-b456-c789-d101112e1310",
+      first_name: "Leslie",
+      last_name: "Backus",
+      display_name: "Leslie Backus",
+      user_name: "leslie_backus",
+      email: "leslie.backus@gmail.com",
+      mobile: "3693279401"
+    },
+    patient_11_Paige_Turner: {
+      id: "11chirop-a123-b456-c789-d101112e1311",
+      first_name: "Paige",
+      last_name: "Turner",
+      display_name: "Paige Turner",
+      user_name: "paige_turner",
+      email: "paige.turner@gmail.com",
+      mobile: "3693279401"
+    },
+    patient_12_Rhea_Adjust: {
+      id: "12chirop-a123-b456-c789-d101112e1312",
+      first_name: "Rhea",
+      last_name: "Adjust",
+      display_name: "Rhea Adjust",
+      user_name: "rhea_adjust",
+      email: "rhea.adjust@gmail.com",
+      mobile: "3693279400"
+    },
+    patient_13_Rolland_Over: {
+      id: "13chirop-a123-b456-c789-d101112e1313",
+      first_name: "Rolland",
+      last_name: "Over",
+      display_name: "Rolland Over",
+      user_name: "rolland_over",
+      email: "rolland.over@gmail.com",
+      mobile: "3693279402"
+    },
+    patient_14_Timberly_Neck: {
+      id: "14chirop-a123-b456-c789-d101112e1314",
+      first_name: "Timberly",
+      last_name: "Neck",
+      display_name: "Timberly Neck",
+      user_name: "timberly_neck",
+      email: "timberly.neck@gmail.com",
+      mobile: "3693279402"
+    },
+    patient_15_Tori_Spinal: {
+      id: "15chirop-a123-b456-c789-d101112e1315",
+      first_name: "Tori",
+      last_name: "Spinal",
+      display_name: "Tori Spinal",
+      user_name: "tori_spinal",
+      email: "tori.spinal@gmail.com",
+      mobile: "8323561584"
+    }
+  }
+};
