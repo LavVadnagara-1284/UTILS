f71446a7c8063c1e8e9cd70ba4fd06eddc3fcfee
Lav Vadnagara <lav.vadnagara@adit.com>
2025-09-05 12:23:22 +0530

    qwerty1

--- ref.js
+++ ref.js
@@ -304,3 +304,199 @@ export const Data = {
 */
 
 // scheduled-list.component.ts > check the timeout changes for stage and stage - demo, once done there then remove the demo condition
+
+
+// This is to update and delete the audio list, this can be used when in need
+/*
+ case "audio_name_update_audio_mock_id_1": {
+   const bName = body.name;
+   const audioId = "audio_mock_id_1";
+
+   const updateAudioName = (dataArray) => {
+     const audioItem = dataArray.find(item => item._id === audioId);
+     if (audioItem) {
+       audioItem.name = bName;
+       audioItem.updated_at = Date.now();
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   updateAudioName(allAudioData);
+   updateAudioName(austinAudioData);
+   updateAudioName(dallasAudioData);
+   updateAudioName(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_name_update_audio_mock_id_2": {
+   const bName = body.name;
+   const audioId = "audio_mock_id_2";
+
+   const updateAudioName = (dataArray) => {
+     const audioItem = dataArray.find(item => item._id === audioId);
+     if (audioItem) {
+       audioItem.name = bName;
+       audioItem.updated_at = Date.now();
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   updateAudioName(allAudioData);
+   updateAudioName(austinAudioData);
+   updateAudioName(dallasAudioData);
+   updateAudioName(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_name_update_audio_mock_id_3": {
+   const bName = body.name;
+   const audioId = "audio_mock_id_3";
+
+   const updateAudioName = (dataArray) => {
+     const audioItem = dataArray.find(item => item._id === audioId);
+     if (audioItem) {
+       audioItem.name = bName;
+       audioItem.updated_at = Date.now();
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   updateAudioName(allAudioData);
+   updateAudioName(austinAudioData);
+   updateAudioName(dallasAudioData);
+   updateAudioName(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_name_update_audio_mock_id_4": {
+   const bName = body.name;
+   const audioId = "audio_mock_id_4";
+
+   const updateAudioName = (dataArray) => {
+     const audioItem = dataArray.find(item => item._id === audioId);
+     if (audioItem) {
+       audioItem.name = bName;
+       audioItem.updated_at = Date.now();
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   updateAudioName(allAudioData);
+   updateAudioName(austinAudioData);
+   updateAudioName(dallasAudioData);
+   updateAudioName(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_delete_audio_mock_id_1": {
+   const audioId = "audio_mock_id_1";
+
+   const deleteAudioItem = (dataArray) => {
+     const audioIndex = dataArray.findIndex(item => item._id === audioId);
+     if (audioIndex !== -1) {
+       dataArray.splice(audioIndex, 1);
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   deleteAudioItem(allAudioData);
+   deleteAudioItem(austinAudioData);
+   deleteAudioItem(dallasAudioData);
+   deleteAudioItem(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_delete_audio_mock_id_2": {
+   const audioId = "audio_mock_id_2";
+
+   const deleteAudioItem = (dataArray) => {
+     const audioIndex = dataArray.findIndex(item => item._id === audioId);
+     if (audioIndex !== -1) {
+       dataArray.splice(audioIndex, 1);
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   deleteAudioItem(allAudioData);
+   deleteAudioItem(austinAudioData);
+   deleteAudioItem(dallasAudioData);
+   deleteAudioItem(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_delete_audio_mock_id_3": {
+   const audioId = "audio_mock_id_3";
+
+   const deleteAudioItem = (dataArray) => {
+     const audioIndex = dataArray.findIndex(item => item._id === audioId);
+     if (audioIndex !== -1) {
+       dataArray.splice(audioIndex, 1);
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   deleteAudioItem(allAudioData);
+   deleteAudioItem(austinAudioData);
+   deleteAudioItem(dallasAudioData);
+   deleteAudioItem(houstonAudioData);
+
+   return response;
+ }
+
+ case "audio_delete_audio_mock_id_4": {
+   const audioId = "audio_mock_id_4";
+
+   const deleteAudioItem = (dataArray) => {
+     const audioIndex = dataArray.findIndex(item => item._id === audioId);
+     if (audioIndex !== -1) {
+       dataArray.splice(audioIndex, 1);
+     }
+   };
+
+   const allAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_all"].data;
+   const austinAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_austin"].data;
+   const dallasAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_dallas"].data;
+   const houstonAudioData = this.mockResponses["audio"]["get"]["dental"]["all"]["audio_houston"].data;
+
+   deleteAudioItem(allAudioData);
+   deleteAudioItem(austinAudioData);
+   deleteAudioItem(dallasAudioData);
+   deleteAudioItem(houstonAudioData);
+
+   return response;
+ }
+*/
